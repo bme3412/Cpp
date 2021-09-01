2a953d8dd159e944afb16d1a7454eeec285313d3
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-08-31 21:02:05 -0400

    Add files via upload

--- Comments/main.cpp
+++ Comments/main.cpp
@@ -1,1 +1,18 @@
+/*
+    Multi-line comments:
+    Lecture Title: The Complete C++ Developer Course
+    Instructor: Dr. J
+*/
 
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 30; // This is my current age
+    double salesTax = 0.06; // This is the MA state sales tax
+    return 0;
+}
