f7f21869cc6fc4efad692cca144c71fd5f9c817a
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-08-31 20:48:15 -0400

    Add files via upload

---
+++ Variables/main3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool isRaining = false;
+    bool isSunny = false;
+    bool isWarm = true;
+
+    cout << boolalpha << isRaining << endl;
+    cout << boolalpha << isSunny << endl;
+    cout << boolalpha << isWarm << endl;
+
+    return 0;
+
+
+}
