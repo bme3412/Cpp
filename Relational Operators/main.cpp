f1785838e885be2bb1a2aa9b5ed14067273489ea
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-04 22:33:34 -0400

    Add files via upload

--- Relational Operators/main.cpp
+++ Relational Operators/main.cpp
@@ -1,1 +1,31 @@
+# include <iostream>>
+using namespace std;
 
+/*
+    > greater tahn
+    >= greater than or equal to
+    < less than
+    <= less than or euqal to
+    == equal to (equality)
+    !+ not-equal-to (equality)
+
+*/
+
+int main()
+{
+
+    cout << boolalpha;
+
+    int a = 15;
+    int b = 20;
+    bool areEqual = a ==b;
+
+    int age = 33;
+
+    cout << (a < b) << endl;
+
+    cout << areEqual << endl;
+
+    cout << "Age >= 21? " << (age >= 21) << endl;
+    return 0;
+}
