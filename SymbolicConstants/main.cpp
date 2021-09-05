0fe636b1fd66a119c0685062e95565e9ddd08208
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-05 09:50:00 -0400

    Add files via upload

--- SymbolicConstants/main.cpp
+++ SymbolicConstants/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+    const double MY_PI = 3.14159;
+    int myTaxRate = 4;
+    const string MY_NAME = "Lionel";
+
+    cout << "Hello " << MY_NAME << endl;
+    return 0;
+}
