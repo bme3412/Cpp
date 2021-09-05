91a08fbf770bcc1f2b2b01ded1152884d60ac098
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-05 19:10:44 -0400

    Add files via upload

--- SelectionControl/main.cpp
+++ SelectionControl/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int age;
+
+    cout<<"Welcome to the Pub and Grille!" << endl;
+    cout << "Please enter your age" << endl;
+    cin >> age;
+
+    if (age >= 21) {
+        cout << "Here, have a beer" << endl;
+    }
+    else if (age >= 16) {
+        cout << "Here have a coke" << endl;
+        cout << "And, at least you can drive, MF" << endl;
+    }
+    
+    else {
+        cout << "Here have a coke" << endl;
+        
+    }
+    cout << "Thanks for coming my dood" << endl;
+    return 0;
+}
