3d17345cc7ddb448421f466d6a0bd72592322a6c
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-05 11:18:22 -0400

    Add files via upload

--- Projects/02_01/main.cpp
+++ Projects/02_01/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() 
+{   double num1;
+    double num2;
+    double num3;
+
+    cout<<"Please enter a real number"<< endl;
+    cin >> num1;
+
+    cout<<"Please enter another real number"<< endl;
+    cin >> num2;
+
+    cout<<"Please enter the final real number"<< endl;
+    cin >> num3;
+
+    double sum = (num1 + num2 + num3);
+
+    cout<<"Average is: " << sum/3 << endl;
+
+
+    return 0;
+}
