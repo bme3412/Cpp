1727a514cdf3ee820427cb9cf13da7a5e7686018
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-06 17:29:24 -0400

    Add files via upload

--- Projects/03_03/main.cpp
+++ Projects/03_03/main.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+
+
+int main () {
+
+    srand(time(nullptr));
+
+    bool guessNumber = false;
+    int guessCount = 0;
+    int ourGuess;
+    int computerNumber = rand() % 100 + 1;
+
+    while (!guessNumber) {
+        cout << "Enter your guess" << endl;
+        cin >> ourGuess;
+
+        if (ourGuess > 100 || ourGuess < 1) {
+            cout << "That was a wasted guess." << endl;
+            guessCount++;
+            continue;
+        }
+
+        if (ourGuess == computerNumber) {
+            cout<<"Congrats! You guessed the number in " << guessCount << " guesses!" << endl;
+            cout << "Thanks for playing!" << endl;
+            guessNumber = true;
+        } 
+        else if (ourGuess > computerNumber) {
+            cout <<"Your guess is too high" << endl;
+        } else {
+            cout <<"Your guess is too low" << endl;
+        }
+
+        guessCount++;
+    }
+
+
+    return 0;
+}
