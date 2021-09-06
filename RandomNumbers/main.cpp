790e49527c364326989cadf511f6573314dbae61
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-09-06 09:49:32 -0400

    Add files via upload

--- RandomNumbers/main.cpp
+++ RandomNumbers/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+int main() {
+    srand(time(nullptr));
+
+    int val1 = rand() % 10; // Val 1 will be >0 and < 10
+
+    // shift it
+    int val2 = rand() % 10 +1;
+
+    cout << val1 << endl;
+    cout << val2 << endl;
+
+
+
+
+    return 0;
+}
